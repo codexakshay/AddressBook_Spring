6c7c00780f34ab411feabca8edefc8da616b356b
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-12-23 09:09:49 +0530

    Added : Create Rest Controller

---
+++ src/main/java/com/bridgelabz/addressBook/dto/AddressBookDTO.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.addressBook.dto;
+
+public class AddressBookDTO {
+	public String firstName;
+	public String lastName;
+	
+	public AddressBookDTO(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressBookDTO [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+}
