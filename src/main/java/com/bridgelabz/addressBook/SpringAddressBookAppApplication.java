f39029e7df341e08e8e6e482678d20d61a63e8fc
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2021-01-07 10:25:02 +0530

    Added : Lombok Library For Logging

---
+++ src/main/java/com/bridgelabz/addressBook/SpringAddressBookAppApplication.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.addressBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class SpringAddressBookAppApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringAddressBookAppApplication.class, args);
+		log.info("Address Book App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+	}
+}
